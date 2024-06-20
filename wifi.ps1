36941a1667b216c036d6b2d7a64213241543bbaa
Xunilone <112978753+Xunilone@users.noreply.github.com>
2024-06-20 16:08:56 +0200

    Add files via upload

---
+++ wifi.ps1
@@ -0,0 +1,21 @@
+$retryCount = 0 # Initialiser le compteur de tentatives à 0
+$wifiName = "SDIS73.local" # Nom du réseau Wi-Fi auquel se connecter
+
+while($retryCount -lt 3) {
+    Start-Sleep -Seconds 5
+    # Vérifiez si nous sommes connectés à un réseau Wi-Fi
+    $connected = Test-Connection -ComputerName $wifiName -Count 1 -Quiet 
+
+    if($connected) {
+        Write-Output "Connecté au Wi-Fi. Fin du script."
+        exit
+    } else {
+        Write-Output "Non connecté au Wi-Fi. Tentative de reconnexion..." 
+        $retryCount++ # Incrémenter le compteur de tentatives
+        # Attendre un peu avant de réessayer
+    }
+}
+
+# Si nous sommes ici, cela signifie que nous n'avons pas réussi à nous connecter après 3 tentatives
+Write-Output "Non connecté au Wi-Fi après 3 tentatives. Redémarrage de l'ordinateur..."
+Restart-Computer -Force
